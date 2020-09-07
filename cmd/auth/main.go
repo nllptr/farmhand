00556b670e7b82adfa83f1b35efc8b9799ff1ee1
Simon Wessel <simon.w.karlsson@gmail.com>
2020-09-07 16:42:42 +0200

    dockerize and composerize :)

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -13,10 +13,28 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// AuthClientID, AuthClientSecret and Host are required environment variables.
+const (
+	AuthClientID     = "AUTH_CLIENT_ID"
+	AuthClientSecret = "AUTH_CLIENT_SECRET"
+	Host             = "HOST"
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("failed to load .env file", err)
+		_, ok := os.LookupEnv(AuthClientID)
+		if !ok {
+			log.Fatal("environment variable AUTH_CLIENT_ID not set")
+		}
+		_, ok = os.LookupEnv(AuthClientSecret)
+		if !ok {
+			log.Fatal("environment variable AUTH_CLIENT_SECRET not set")
+		}
+		_, ok = os.LookupEnv(Host)
+		if !ok {
+			log.Fatal("environment variable HOST not set")
+		}
 	}
 	ctx := context.Background()
 	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
@@ -24,9 +42,9 @@ func main() {
 		log.Panic("failed to create OIDC provider: ", err)
 	}
 	config := &oauth2.Config{
-		ClientID:     os.Getenv("AUTH_CLIENT_ID"),
-		ClientSecret: os.Getenv("AUTH_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("HOST") + "/auth/callback",
+		ClientID:     os.Getenv(AuthClientID),
+		ClientSecret: os.Getenv(AuthClientSecret),
+		RedirectURL:  os.Getenv(Host) + "/auth/callback",
 		Endpoint:     provider.Endpoint(),
 		// "openid" is a required scope for OpenID Connect flows.
 		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
--- docker/auth.Dockerfile
+++ docker/auth.Dockerfile
@@ -4,9 +4,9 @@ COPY go.mod .
 COPY go.sum .
 RUN go mod download
 COPY . .
-RUN go build ./cmd/auth
+RUN CGO_ENABLED=0 GOOS=linux go build ./cmd/auth
 
 FROM alpine:latest  
 WORKDIR /root/
 COPY --from=builder /build/auth .
-CMD ["./app"]  
+CMD ["./auth"]  
